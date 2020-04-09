8480690d44e3079a9fffc21040d26f98b695ecab
thaliastefani <thaliastefany.tt@gmail.com>
2020-04-09 00:53:29 -0300

    adiciona exercicio 14

--- LPEx14/main.c
+++ LPEx14/main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
-    int matriz[9][9], vetor[81], i, j, numAleatorio, auxiliar;
+    int matriz[9][9], vetor[81], i, j, numAleatorio, auxiliar, contador = 0;
 
     srand((unsigned)(time(NULL)));
 
     for (i = 0; i < 9; i++){
         printf("\n");
         for (j = 0; j < 9; j++){
-            numAleatorio = (rand() % 99);
+            numAleatorio = (rand() % 500);
             if ((numAleatorio % 2) == 0){
                 numAleatorio += 1;
             }
             matriz[i][j] = numAleatorio;
-            vetor[j] = matriz[i][j];
+            vetor[contador] = matriz[i][j];
+            contador++;
         }
     }
+
     for (i = 0; i < 81; i++){
         for (j = 0; j < 81; j++){
-            if(vetor[i] < vetor[i+1]){
+            if(vetor[i] < vetor[j]){
                 auxiliar = vetor[i];
                 vetor[i] = vetor[j];
                 vetor[j] = auxiliar;
             }
         }
     }
+
     for (i = 0; i < 81; i++){
         printf("%d\n", vetor[i]);
     }
